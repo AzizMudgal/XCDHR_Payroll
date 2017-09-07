9248b66e00b967c9644ff5034209472d7a368cb4
Swamy100817 <nanjundas@xcdhr.com>
2017-09-07 17:08:21 +0530

    Code Merge Changes are made as per Maven upgrade

--- Salesforce_Core_Framework/src/main/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payroll/NI_FourweeklyCatK/TestReports.java
+++ Salesforce_Core_Framework/src/main/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payroll/NI_FourweeklyCatK/TestReports.java
@@ -1,4 +1,4 @@
-package com.test.xcdhr.Salesforce_Core_Framework1.hrms_payroll.NI_FourweeklyCatA;
+package com.test.xcdhr.Salesforce_Core_Framework1.hrms_payroll.NI_FourweeklyCatK;
 
 
 import java.io.File;
@@ -27,7 +27,6 @@ import atu.webdriver.utils.table.WebTable;
 
 import com.test.xcdhr.Salesforce_Core_Framework1.Salesforce_Util.Test_Util;
 
-
 public class TestReports extends TestSuiteBase
 {
 	String runmodes[] = null;
@@ -42,17 +41,17 @@ public class TestReports extends TestSuiteBase
 	public void CheckTestSkip() throws Throwable
 	{
 		processDesiredTaxYearInputExcelFile(TaxYear);
-		if(! Test_Util.IsTestcaseRunMode(Payroll_4WeeklyCatA_SuiteXls, this.getClass().getSimpleName()))
+		if(! Test_Util.IsTestcaseRunMode(Payroll_4WeeklyCatK_SuiteXls, this.getClass().getSimpleName()))
 		{
 			Skip=true;
 			Test_Util.ReportDataSetResult(
-					Payroll_4WeeklyCatA_SuiteXls, "first", Test_Util.GetRowNum(Payroll_4WeeklyCatA_SuiteXls, this.getClass().getSimpleName()),"Skipped");
-			//Test_Util.ReportDataSetResult(Payroll_4WeeklyCatA_SuiteXls, this.getClass().getSimpleName(), count+2, "Skip");
+					Payroll_4WeeklyCatK_SuiteXls, "first", Test_Util.GetRowNum(Payroll_4WeeklyCatK_SuiteXls, this.getClass().getSimpleName()),"Skipped");
+			//Test_Util.ReportDataSetResult(Payroll_4WeeklyCatK_SuiteXls, this.getClass().getSimpleName(), count+2, "Skip");
 			APP_LOGS.debug("skipping the testcase" +this.getClass().getSimpleName() +" as the runmode is set to 'no' ");// this message would display in logs
 			throw new Exception("Testcase is being skipped" + this.getClass().getSimpleName()+ "as it's Runmode is set to 'NO'"); // this msg would display in Reports.
 		}
 		// Load the runmodes of the tests
-		runmodes=Test_Util.getDataSetRunmodes(Payroll_4WeeklyCatA_SuiteXls, this.getClass().getSimpleName());
+		runmodes=Test_Util.getDataSetRunmodes(Payroll_4WeeklyCatK_SuiteXls, this.getClass().getSimpleName());
 	}
 
 
@@ -69,7 +68,7 @@ public class TestReports extends TestSuiteBase
 		openBrowser();
 		logingIntoDesiredORG(OrgFlag);
 		driver.manage().window().maximize();
-				
+		
 		try
 		{
 			titlename = driver.getTitle();
@@ -299,7 +298,7 @@ public class TestReports extends TestSuiteBase
 	public Object[][] getData() throws Throwable
 	{
 		processDesiredTaxYearInputExcelFile(TaxYear);
-		return Test_Util.getData(Payroll_4WeeklyCatA_SuiteXls,"TestReports");
+		return Test_Util.getData(Payroll_4WeeklyCatK_SuiteXls,"TestReports");
 	}
 
 
@@ -310,16 +309,16 @@ public class TestReports extends TestSuiteBase
 		processDesiredTaxYearInputExcelFile(TaxYear);
 		if(Skip)
 		{
-			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatA_SuiteXls, this.getClass().getSimpleName(), count+2, "Skip");
+			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatK_SuiteXls, this.getClass().getSimpleName(), count+2, "Skip");
 		}
 		else if(Fail)
 		{
 			IsTestPass = false;
-			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatA_SuiteXls, this.getClass().getSimpleName(), count+2, "Fail");
+			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatK_SuiteXls, this.getClass().getSimpleName(), count+2, "Fail");
 		}
 		else
 		{
-			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatA_SuiteXls, this.getClass().getSimpleName(), count+2, "Pass");
+			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatK_SuiteXls, this.getClass().getSimpleName(), count+2, "Pass");
 		}
 		Skip=false;
 		Fail=false;
@@ -335,11 +334,11 @@ public class TestReports extends TestSuiteBase
 		{
 			// This will update the testresult in the first worksheet where in for that test case , even if one of the test data specified in second worksheet fails, the test 
 			// would be considered as fail.And the same would be updated.
-			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatA_SuiteXls, "first", Test_Util.GetRowNum(Payroll_4WeeklyCatA_SuiteXls, this.getClass().getSimpleName()),"Pass");
+			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatK_SuiteXls, "first", Test_Util.GetRowNum(Payroll_4WeeklyCatK_SuiteXls, this.getClass().getSimpleName()),"Pass");
 		}
 		else
 		{
-			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatA_SuiteXls, "first", Test_Util.GetRowNum(Payroll_4WeeklyCatA_SuiteXls, this.getClass().getSimpleName()),"Fail");
+			Test_Util.ReportDataSetResult(Payroll_4WeeklyCatK_SuiteXls, "first", Test_Util.GetRowNum(Payroll_4WeeklyCatK_SuiteXls, this.getClass().getSimpleName()),"Fail");
 		}
 		System.out.println("closing the browser");
 		closeBrowser();
