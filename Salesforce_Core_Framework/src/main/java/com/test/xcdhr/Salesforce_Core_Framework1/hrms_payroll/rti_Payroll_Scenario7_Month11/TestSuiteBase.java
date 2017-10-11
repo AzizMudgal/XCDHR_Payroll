ff09e84dd7b53e602f9258157271b64dfb815ddb
Swamy100817 <nanjundas@xcdhr.com>
2017-10-11 11:49:00 +0530

    Code Changes as part of RTI Scenario 7 Creation

---
+++ Salesforce_Core_Framework/src/main/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payroll/rti_Payroll_Scenario7_Month11/TestSuiteBase.java
@@ -0,0 +1,23 @@
+package com.test.xcdhr.Salesforce_Core_Framework1.hrms_payroll.rti_Payroll_Scenario7_Month11;
+
+import org.testng.annotations.BeforeSuite;
+
+import com.test.xcdhr.Salesforce_Core_Framework1.testBase.TestBase;
+import com.test.xcdhr.Salesforce_Core_Framework1.Salesforce_Util.Test_Util;
+
+public class TestSuiteBase extends TestBase
+{
+	@BeforeSuite
+	public void CheckSuiteSkip() throws Throwable
+	{
+		initialize();
+		processDesiredTaxYearInputExcelFile(TaxYear);
+		APP_LOGS.debug("Checking runmode of"+ PayrollRecognitionScenario7_Inputsheet);
+		if(! Test_Util.isSuiteRunnable(SuiteXls,PayrollRecognitionScenario7_Inputsheet))
+		{
+			APP_LOGS.debug("Setting the Payroll Suite to OFF as the runmode is set to 'N'");
+			throw new Exception("Payroll suite is not going to execute as its being skipped");
+		}
+	}
+
+}
