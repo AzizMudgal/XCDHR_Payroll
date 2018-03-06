e89d1ead00d1e8b39f0cff40ddbf25591b306965
Swamy100817 <nanjundas@xcdhr.com>
2018-03-06 12:07:35 +0530

    NI-Weekly files checkin

--- Salesforce_Core_Framework/src/main/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payroll/NI_FourweeklyCatA/ProcessPayrollForNIWeekly.java
+++ Salesforce_Core_Framework/src/main/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payroll/NI_FourweeklyCatA/ProcessPayrollForNIWeekly.java
@@ -95,12 +95,7 @@ public class ProcessPayrollForNIWeekly extends TestSuiteBase
 
 				System.out
 				.println("The test script logged in successfully into salesforce account");
-<<<<<<< HEAD
 				System.out.println("");
-=======
-				System.out.println("");
-
->>>>>>> refs/remotes/origin/master
 				PayrollForWeeklyTax(EmployerName,EmpName,Payrolid,Frquency,WeekName,ExcelInputSheet,FirstReportNameInApplication,TestResultExcelFilePath,worksheetNo,PayrollVeiw);
 			}
 			catch (Throwable t)
