3838f3c4a3edb28e14d1946b765f7d01d5569ec3
Admin <Admin@192.168.1.124>
2018-05-02 19:11:07 +0530

    commit changes

--- Salesforce_Core_Framework/src/main/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payroll/AutoEnrollment_Initial_Setup/CreatePayrolls.java
+++
@@ -1,5 +0,0 @@
-package com.test.xcdhr.Salesforce_Core_Framework1.hrms_payroll.AutoEnrollment_Initial_Setup;
-
-public class CreatePayrolls {
-
-}
--- Salesforce_Core_Framework/src/main/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payroll/Reset_Predefined_Conditions_Taxyear201718/ResetEffectveFrmDateForWeekly.java
+++
@@ -1,5 +0,0 @@
-package com.test.xcdhr.Salesforce_Core_Framework1.hrms_payroll.Reset_Predefined_Conditions_Taxyear201718;
-
-public class ResetEffectveFrmDateForWeekly {
-
-}
