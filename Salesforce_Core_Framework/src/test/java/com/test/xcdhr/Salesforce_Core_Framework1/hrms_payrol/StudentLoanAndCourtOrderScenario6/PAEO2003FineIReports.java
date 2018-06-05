2d77bb2ecf9d923ee3f2560f26d13a5d9dbdad70
AzizMudgal <azizm@peoplexcd.com>
2018-06-05 15:26:26 +0530

    committing files

--- Salesforce_Core_Framework/src/test/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payrol/StudentLoanAndCourtOrderScenario6/PAEO2003FineIReports.java
+++ Salesforce_Core_Framework/src/test/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payrol/StudentLoanAndCourtOrderScenario6/PAEO2003FineIReports.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 import com.test.xcdhr.Salesforce_Core_Framework1.Salesforce_Util.Test_Util;
 
 
-public class CAPSReports extends CourtOrderAEO1971civildebt
+public class PAEO2003FineIReports extends CourtOrderAEO1971civildebt
 {
 	String runmodes[] = null;
 	static int count = -1;
