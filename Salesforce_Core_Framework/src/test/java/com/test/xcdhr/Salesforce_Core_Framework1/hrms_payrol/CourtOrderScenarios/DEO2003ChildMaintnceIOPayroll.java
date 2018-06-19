fa6c6a665a4d19cec7645edc4914ac4ac2fa4ca4
AzizMudgal <azizm@peoplexcd.com>
2018-06-19 19:06:54 +0530

    committing files

--- Salesforce_Core_Framework/src/test/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payrol/CourtOrderScenarios/DEO2003ChildMaintnceIOPayroll.java
+++ Salesforce_Core_Framework/src/test/java/com/test/xcdhr/Salesforce_Core_Framework1/hrms_payrol/CourtOrderScenarios/DEO2003ChildMaintnceIOPayroll.java
@@ -1,7 +1,6 @@
 package com.test.xcdhr.Salesforce_Core_Framework1.hrms_payrol.CourtOrderScenarios;
 
 import com.test.xcdhr.Salesforce_Core_Framework1.hrms_payroll.CourtOrderActionsPackage.*;
-
 import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
@@ -9,7 +8,6 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import com.test.xcdhr.Salesforce_Core_Framework1.Salesforce_Util.Test_Util;
 
 
@@ -71,10 +69,10 @@ public class DEO2003ChildMaintnceIOPayroll extends CourtOrderSuperClass
 		}
 		System.out.println("Runmode for Test set data is set to YES hence Executing the test case to select the desired Org");
 		System.out.println("");
+		
 		/*
 		 * The Org based on the selection at base class would be invoked.
 		 */
-
 		toSelectDesiredOrg(OrgFlag);
 	}
 
